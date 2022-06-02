71133393c65a003ac964e18989963d5761538107
Nirwash <nirwash@bk.ru>
2022-06-02 06:40:54 +0300

    create string resources create incrementScore()

--- app/src/main/java/com/nirwashh/android/timefighter/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/timefighter/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nirwashh.android.timefighter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nirwashh.android.timefighter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var b: ActivityMainBinding
+    private var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        b.btnTapMe.setOnClickListener { incrementScore() }
     }
+
+    private fun incrementScore(){
+        score++
+        val newScore = getString(R.string.your_score, score)
+        b.tvGameScore.text = newScore
+    }
+
+    private fun resetGame() {}
+
+    private fun startGame() {}
+
+    private fun endGame() {}
 }
