6b9ac0946d5dc49d467e58497a37f2aea1d152dd
Nirwash <nirwash@bk.ru>
2022-06-07 14:17:40 +0300

    сохранение state

--- app/src/main/java/com/nirwashh/android/timefighter/MainActivity.kt
+++ app/src/main/java/com/nirwashh/android/timefighter/MainActivity.kt
@@ -19,7 +19,40 @@ class MainActivity : AppCompatActivity() {
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
         b.btnTapMe.setOnClickListener { incrementScore() }
+        if (savedInstanceState != null) {
+            score = savedInstanceState.getInt(SCORE_KEY)
+            timeLeft = savedInstanceState.getInt(TIME_LEFT_KEY)
+            restoreGame()
+        } else {
         resetGame()
+        }
+    }
+
+    private fun restoreGame() {
+        val restoredScore = getString(R.string.your_score, score)
+        b.tvGameScore.text = restoredScore
+        val restoredTime = getString(R.string.time_left, timeLeft)
+        b.tvTimeLeft.text = restoredTime
+        countDownTimer = object : CountDownTimer((timeLeft * 1000).toLong(), countDownInterval) {
+            override fun onTick(p0: Long) {
+                timeLeft = p0.toInt() / 1000
+                val timeLeftString = getString(R.string.time_left, timeLeft)
+                b.tvTimeLeft.text = timeLeftString
+            }
+
+            override fun onFinish() {
+               endGame()
+            }
+        }
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeLeft)
+        countDownTimer.cancel()
     }
 
     private fun incrementScore(){
@@ -62,4 +95,10 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, getString(R.string.game_over_message, score), Toast.LENGTH_SHORT).show()
         resetGame()
     }
+
+    companion object {
+        private const val SCORE_KEY = "score key"
+        private const val TIME_LEFT_KEY = "time left key"
+    }
+
 }
